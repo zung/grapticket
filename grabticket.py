b41e4fa471476c233b8cd2e5d49b72f57543b109
bearq <zung435@gmail.com>
2017-11-13 17:31:33 +0800

    '更新stations信息;添加车票订单文件'

--- grabticket.py
+++ grabticket.py
@@ -1,8 +1,9 @@
 import json
 import random
 import re
-from urllib.parse import quote
+from urllib.parse import quote, unquote
 
+import datetime
 import requests
 import urllib3
 from PIL import Image
@@ -13,7 +14,7 @@ from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(InsecureRequestWarning)
 
 _data = {
-    'ctx':'/otn/',
+    'ctx': '/otn/',
     'globalRepeatSubmitToken': '',
     'global_lang': 'zh_CN',
     'sessionInit': '',
@@ -29,7 +30,7 @@ _data = {
     'passport_authclient': 'uamauthclient',
     'passport_loginPage': 'login/init',
     'passport_okPage': 'index/initMy12306',
-    'passport_proxy_captcha':  'login/init'
+    'passport_proxy_captcha': 'login/init'
 }
 
 headers = {
@@ -127,7 +128,7 @@ def uampasswort(tk):
     print(res.text)
     msg = json.loads(res.text)
 
-    if msg['result_code'] == 0: # 验证通过
+    if msg['result_code'] == 0:  # 验证通过
         # 跳转到指定页面
         get_left_ticket()
 
@@ -138,10 +139,10 @@ def uampasswort(tk):
 def check_user():
     """验证用户"""
     url = 'https://kyfw.12306.cn/otn/login/checkUser'
-    data = {'_json_att':''}
+    data = {'_json_att': ''}
     res = session.post(url, data=data, verify=False, headers={'If-Modified-Since': '0',
-                                                   'Cache-Control': 'no-cache',
-                                                   'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'})
+                                                              'Cache-Control': 'no-cache',
+                                                              'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'})
     msg = json.loads(res.text)
     print('验证用户')
     print(msg)
@@ -155,14 +156,14 @@ def check_user():
 
 
 def submit_order(t, _f, _t):
-    """提交订单"""
+    """提交预订单"""
 
     url = 'https://kyfw.12306.cn/otn/leftTicket/submitOrderRequest'
     trainDate = str2date_format1(t['出发日'])
     backDate = trainDate
 
     data = {
-        "secretStr": t['secretStr'],
+        "secretStr": unquote(t['secretStr']),
         "train_date": trainDate,
         "back_train_date": backDate,
         "tour_flag": "dc",
@@ -175,7 +176,7 @@ def submit_order(t, _f, _t):
     if not check_user():
         return False
 
-    res = session.post(url, data=data, verify=False)
+    res = session.post(url, data=data, headers={"Content-Type": "application/x-www-form-urlencoded"}, verify=False)
 
     msg = json.loads(res.text)
     print(msg)
@@ -191,16 +192,150 @@ def submit_order(t, _f, _t):
 
 
 def confirm_passenger():
+    """确认乘客信息"""
     url = 'https://kyfw.12306.cn/otn/confirmPassenger/initDc'
     res = session.get(url, verify=False)
-    print(res.text)
+    # print(res.text)
+
+    token_pattern = r'var globalRepeatSubmitToken = \'(.*?)\';'
+    token = re.findall(token_pattern, res.text)[0]
+
+    pattern = r'var ticketInfoForPassengerForm=(.*?);'
+    ticketInfoStr = re.findall(pattern, res.text)[0]  # 车票信息
+    ticketInfoStr = ticketInfoStr.replace('\'', '"')
+    ticketInfoJson = json.loads(ticketInfoStr)
+    print(json.dumps(ticketInfoJson, sort_keys=True, indent=2, ensure_ascii=False))
+
+    trainInfo = ticketInfoJson['queryLeftTicketRequestDTO']  # 列车信息
+    leftTicketStr = ticketInfoJson['leftTicketStr']
+    train_date = trainInfo['train_date']
+
+    # 星期几
+    weekday = get_weekday_by_date(datetime.date(int(train_date[:4]), int(train_date[4:6]), int(train_date[6:8])))
+
+    print("列车信息(以下余票信息仅供参考)")
+    print(str2date_format1(train_date) + '（' + weekday + '） ' + trainInfo['station_train_code'] + '次 ' \
+          + trainInfo['from_station_name'] + '站（' + trainInfo['start_time'] + '开）' \
+          + "--" + trainInfo['to_station_name'] + '站（' + trainInfo['arrive_time'] + '到）')
+    print('-----------------------------------------------------------------')
+    leftDetails = ticketInfoJson['leftDetails']
+    for det in leftDetails:
+        print(det + '\t', end='')
+
+    # 获取乘客信息
+    normal_passengers = get_passengers(token)
+    if normal_passengers:
+        print("乘客信息")
+        for passenger in normal_passengers:
+            print('1.' + passenger['passenger_name'])
+
+    while True:
+        k = input('选择乘客，请输入%d - %d之间的数字来选择车次：' % (1, len(normal_passengers)))
+        if not k.isdigit():
+            print('请输入数值类型！')
+            continue
+        k = int(k)
+        if k < 1 or k > len(normal_passengers):
+            print('输入有误!请重新输入')
+            continue
+
+        p = normal_passengers[k - 1]
+        break
+
+    passengerTicketStr = get_passenger_tickets()
+
+    formData = {
+
+    }
+
+    # 验证订单
+    flag = check_order_info(token, passengerTicketStr, oldPassengerStr=None, tour_flag=None)
+    if flag:  # 验证成功
+        queueCountData = get_queue_count(formData)  # 获取队列计数
+        # 提交订单
+
+    else:
+        flag = False
+
+
+def get_queue_count(data):
+    """获取队列计数"""
+    url = 'https://kyfw.12306.cn/otn/confirmPassenger/getQueueCount'
+    res = session.post(url, data=data, verify=False)
+    msg = json.loads(res.text)
+    if msg['status']:
+        return msg['data']
+    else:
+        print(msg['messages'])
+        return False
+
+
+def get_weekday_by_date(date):
+    """根据日期获取当前是星期几"""
+    weekday = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    return weekday[date.weekday()]
+
+
+def get_standard_time(date):
+    """获取标准时间"""
+    return date.strftime('%a %b %Y %H:%M:%S ') + 'GMT+0800 (中国标准时间)'
+
+
+def get_passenger_tickets(limitTickets):
+    str = ''
+    for v in limitTickets:
+        av = v['seat_type'] + ',0,' + v['ticket_type'] + ',' + v['name'] + ',' \
+             + v['id_type'] + ',' + v['id_no'] + ',' + v['phone_no'] + ',' \
+             + 'N' if v['save_status'] == '' else 'Y'
+        str += av + '_'
+    return str[:len(str) - 1]
+
+
+def get_old_passengers(ticketInfoStr, limitTickets):
+    str = ''
+    for v in limitTickets:
+        pass
+
+
+def limit_tickets():
+    """获取限购车票的列表"""
+    pass
+
+
+def ticket_submit_order():
+    """获取车票提交订单信息"""
+    tso = json.load(open('ticket_submit_order.json', 'r', encoding='utf-8'))
+    print(tso)
+
+
+def check_order_info(token, passengerTicketStr, oldPassengerStr, tour_flag):
+    """验证订单信息"""
+    url = 'https://kyfw.12306.cn/otn/confirmPassenger/checkOrderInfo'
+    data = {
+        'cancel_flag': '2',
+        'bed_level_order_num': '000000000000000000000000000000',
+        'passengerTicketStr': passengerTicketStr,
+        'oldPassengerStr': oldPassengerStr,
+        'tour_flag': tour_flag,
+        'randCode': '',
+        '_json_att': '',
+        'REPEAT_SUBMIT_TOKEN': token
+    }
+
+    res = session.post(url, data=data, verify=False)
+    msg = json.loads(res.text)
+    if msg['status'] and msg['data']['submitStatus']:
+        return True
+    else:
+        print(msg['messages'])
+        return False
 
 
 def str2date_format1(str):
     """字符串转日期格式 yyyy-MM-dd"""
     l = list(str)
     l.insert(4, '-')
-    l.insert(-2,'-')
+    l.insert(-2, '-')
     return ''.join(l)
 
 
@@ -216,14 +351,21 @@ def get_captcha():
     check_captcha()
 
 
-def get_passengers():
-    """获取联系人"""
+def get_passengers(token):
+    """获取乘客信息"""
     url = 'https://kyfw.12306.cn/otn/confirmPassenger/getPassengerDTOs'
-    res = session.get(url, verify=False)
+    data = {
+        '_json_att': '',
+        'REPEAT_SUBMIT_TOKEN': token
+    }
+    res = session.post(url, data=data, verify=False)
     html = res.text
     msg = json.loads(html)
     if msg['status'] and msg['data']['isExist']:
         return msg['data']['normal_passengers']
+    else:
+        print("没有获取到乘客信息")
+        return None
 
 
 def get_pass_code():
@@ -235,8 +377,8 @@ def get_pass_code():
 
 
 def get_left_ticket_log(d, f, t):
-    url = 'https://kyfw.12306.cn/otn/leftTicket/log?leftTicketDTO.train_date=' + d + '&leftTicketDTO.from_station='\
-        + f + '&leftTicketDTO.to_station=' + t + '&purpose_codes=ADULT'
+    url = 'https://kyfw.12306.cn/otn/leftTicket/log?leftTicketDTO.train_date=' + d + '&leftTicketDTO.from_station=' \
+          + f + '&leftTicketDTO.to_station=' + t + '&purpose_codes=ADULT'
     res = session.get(url, verify=False)
     msg = json.loads(res.text)
     print(msg)
@@ -259,8 +401,9 @@ def get_left_ticket():
     _from = get_code_by_input(f)
     _to = get_code_by_input(t)
 
-    fromStation = quote(get_code_by_input(_from) + ',' + _from)
-    toStation = quote(get_code_by_input(_to) + ',' + _to)
+    fromStation = quote(get_code_by_input(_from) + ',' + _from).replace('\\', '%')
+    toStation = quote(get_code_by_input(_to) + ',' + _to).replace('\\', '%')
+    print(fromStation)
 
     cookies = {
         "_jc_save_fromDate": d,
@@ -268,12 +411,9 @@ def get_left_ticket():
         "_jc_save_toDate": d,
         "_jc_save_toStation": toStation,
         "_jc_save_wfdc_flag": "dc",
-        "current_captcha_type": 'Z'
     }
 
     requests.utils.add_dict_to_cookiejar(session.cookies, cookies)
-
-    print(cookies)
 
     # ck = requests.utils.cookiejar_from_dict(cookies)
     # session.cookies = ck
@@ -281,8 +421,8 @@ def get_left_ticket():
     if not get_left_ticket_log(d, _from, _to):
         return None
 
-    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=' + d + '&leftTicketDTO.from_station='\
-        + _from +'&leftTicketDTO.to_station=' + _to + '&purpose_codes=ADULT'
+    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=' + d + '&leftTicketDTO.from_station=' \
+          + _from + '&leftTicketDTO.to_station=' + _to + '&purpose_codes=ADULT'
     res = session.get(url, verify=False)
     msg = json.loads(res.text)
     print(json.dumps(msg, sort_keys=True, indent=2, ensure_ascii=False))
@@ -319,8 +459,8 @@ def get_left_ticket():
         for i, c in enumerate(cols):
             if i == 0 or i == 12:
                 continue
-            print('%s:\t%s'%(i + 1, c), end=' ')
-            if (i+1) % 9 == 0:
+            print('%s:\t%s' % (i + 1, c), end=' ')
+            if (i + 1) % 9 == 0:
                 print('')
         print('')
 
@@ -358,11 +498,11 @@ def get_station_names():
             stations[s[2]]['汉字'] = s[1]
             stations[s[2]]['简拼'] = s[0]
             stations[s[2]]['全拼'] = s[3]
-    json.dump(stations, open('stations.json', 'w'))
+    json.dump(stations, open('stationsb.json', 'w'))
 
 
 def get_code_by_input(value):
-    stations = json.load(open('stations.json', 'r'))
+    stations = json.load(open('stationsb.json', 'r'))
     for key in stations.keys():
         if value in stations[key].values():
             return key
@@ -374,3 +514,4 @@ if __name__ == '__main__':
     index()
     # get_left_ticket()
     # get_station_names()
+    # ticket_submit_order()
